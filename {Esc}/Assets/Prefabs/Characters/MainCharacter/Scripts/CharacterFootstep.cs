352422a5bdc28619b885b6e684cef811bb5a9c08
amatree <tri.tran.pmmp@gmail.com>
2022-12-05 16:37:38 -0800

    character footstep wip

---
+++ {Esc}/Assets/Prefabs/Characters/MainCharacter/Scripts/CharacterFootstep.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFootstep : MonoBehaviour
+{
+	[Header("Dependencies")]
+	public Animator characterAnimator;
+	public AudioSource audioSource;
+	public ErikaArcherAnimationHandle animationHandler;
+
+	[Header("Clips")]
+	public AudioClip walkingClip;
+	public AudioClip runningClip;
+
+	Coroutine footstepClipC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterAnimator is not null)
+		{
+			if (characterAnimator.GetBool(animationHandler.isWalkingParameterName))
+			{
+			 	footstepClipC = StartCoroutine(PlayClip(walkingClip));
+			} else if (characterAnimator.GetBool(animationHandler.isSprintingParameterName))
+			{ 
+				footstepClipC = StartCoroutine(PlayClip(runningClip));
+			} else {
+				if  (footstepClipC is not null) StopCoroutine(footstepClipC);
+				if (audioSource.clip == walkingClip || audioSource.clip == runningClip) audioSource.clip = null;
+			}
+		}
+    }
+
+	IEnumerator PlayClip(AudioClip clip)
+	{
+		if (audioSource.clip != clip) audioSource.clip = clip;
+		audioSource.Play();
+		yield return new WaitForSeconds(clip.length);
+	}
+}
